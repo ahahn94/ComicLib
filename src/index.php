c90c7d940f5beacafb4c9e8062085306edc507b2
ahahn94 <ahahn94@outlook.com>
2019-05-09 19:34:04 +0200

    Add Classes For Database Tables
    Add the classes for the database tables.
    Add the add-function to these classes.
    Add testing of these functions to index.php.
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- src/index.php
+++ src/index.php
@@ -4,31 +4,45 @@
  * on 03.05.19
  */
 
-require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/Database/Management/Connection.php";
-require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/Logging/Logging.php";
-require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/ComicVineAPI/Management/APICall.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/Database/Resources/Publishers.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/Database/Resources/Volumes.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/Database/Resources/Issues.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/ComicVineAPI/Resources/Publisher.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/ComicVineAPI/Resources/Volume.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/ComicVineAPI/Resources/VolumeIssue.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/ComicVineAPI/Resources/Issue.php";
 
+/*
+ * Test adding to database tables.
+ */
 
-// Test request of publisher.
-$publisher = Publisher::get("31");
-print_r($publisher);
-print "<br>";
+// Test database for publishers.
+$publisherList = array("10", "31");
+$publishers = new Publishers();
+foreach ($publisherList as $publisher) {
+    $dataset = Publisher::get($publisher);
+    $publishers->add($dataset);
+}
 
-// Test request of volume.
-$volume = Volume::get("110496");
-print_r($volume);
-print "<br>";
+// Test database for volumes.
+$volumesList = array("110496", "91273", "111428", "111704");
+$volumes = new Volumes();
+foreach ($volumesList as $volume) {
+    $dataset = Volume::get($volume);
+    $volumes->add($dataset);
+}
 
-// Test request of volumeIssues.
-$volumeIssues = VolumeIssue::get("110496");
-print_r($volumeIssues);
-print "<br>";
-
-// Test request of issues.
-$issue = Issue::get("668770");
-print_r($issue);
-print "<br>";
+// Test database for issues.
+$issues = new Issues();
+foreach ($volumesList as $volume) {
+    // Limit issue count to 5 per volume.
+    $counter = 0;
+    $maxCount = 5;
+    $volumeIssues = VolumeIssue::get($volume);
+    foreach ($volumeIssues["Issues"] as $volumeIssue) {
+        $dataset = Issue::get($volumeIssue["IssueID"]);
+        $issues->add($dataset);
+        $counter++;
+        if ($counter == $maxCount) break;
+    }
+}
---
+++ src/php_includes/Database/Resources/Issues.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Created by ahahn94
+ * on 05.05.19
+ */
+
+require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/Database/Resources/Table.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/Database/Management/Connection.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/Logging/Logging.php";
+
+/**
+ * Class Issues
+ * Implements operations on the ComicLib/Issues table.
+ */
+class Issues implements Table
+{
+
+    private $connection = null; // Database connection.
+
+    /**
+     * Issues constructor.
+     */
+    public function __construct()
+    {
+        $this->connection = Connection::getInstance();
+    }
+
+    /**
+     * Get the dataset identified by the provided ID.
+     * @param $id string ID of the dataset.
+     * @return array Dataset. Empty array if not found.
+     */
+    public function get($id)
+    {
+        // TODO: Implement get() method.
+        return array();
+    }
+
+    /**
+     * Get all datasets of the table.
+     * @return array All datasets as an array of arrays. Empty array if not found.
+     */
+    public function getAll()
+    {
+        // TODO: Implement getAll() method.
+        return array();
+    }
+
+    /**
+     * Add a new dataset to the table.
+     * @param $dataset array New dataset.
+     * @return int 0 if ok, else MySQL error code.
+     */
+    public function add($dataset)
+    {
+        $statement = "INSERT INTO Issues (IssueID, VolumeID, APIDetailURL, Description, ImageURL, IssueNumber, Name) " .
+            "VALUES (:IssueID, :VolumeID, :APIDetailURL, :Description, :ImageURL, :IssueNumber, :Name)";
+        $query = $this->connection->prepare($statement);
+
+        try {
+            $query->execute($dataset);
+        } catch (Exception $e) {
+            // Error handling if error while writing to database.
+            $errorMessage = "Error writing Issue {IssueID = " . $dataset["IssueID"] . "} to database!";
+            Logging::logError($errorMessage);
+            print($errorMessage . "<br>");
+            Logging::logError($e->getMessage());
+            print($e->getMessage() . "<br>");
+        }
+
+        return $query->errorCode();
+    }
+
+    /**
+     * Update a dataset.
+     * @param $dataset array Dataset with the updated data and the ID of the dataset to change.
+     * @return int 0 if ok, else MySQL error code.
+     */
+    public function update($dataset)
+    {
+        // TODO: Implement update() method.
+        return 0;
+    }
+
+    /**
+     * Remove the dataset identified by the provided ID.
+     * @param $id string ID of the dataset to delete.
+     * @return int 0 if ok, else MySQL error code.
+     */
+    public function remove($id)
+    {
+        // TODO: Implement remove() method.
+        return 0;
+    }
+}
