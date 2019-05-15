ab68f61d4b8ef0a10617114b41acb15a7ce9b69d
ahahn94 <ahahn94@outlook.com>
2019-05-15 16:16:36 +0200

    Add 404 Not Found View
    Add view for the "404 Not Found" error:
    - add message and icon to page center via Bootstrap and FontAwesome
    - add background image (from pexels.com)
    - optimize layout for both mobile and desktop
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- src/php_includes/Views/NotFoundView.php
+++ src/php_includes/Views/NotFoundView.php
@@ -9,5 +9,40 @@
  */
 
 ?>
+<html lang="en">
+<header>
+    <?php
+    /*
+     * Include the headers for Bootstrap and FontAwesome.
+     */
+    require_once $_SERVER["DOCUMENT_ROOT"] . "/resources/html/BootstrapHeader.html";
+    require_once $_SERVER["DOCUMENT_ROOT"] . "/resources/html/FontAwesomeHeader.html";
+    ?>
+</header>
+<body>
 
-<h1>Site not found!</h1>
+<!-- Full-screen background. -->
+<div style='background: url("/resources/images/NotFoundBackground.jpg") no-repeat center center fixed; background-size:
+cover; position: fixed; height: 100%; width: 100%;'>
+
+    <div class="text-center text-light" style="min-height: 100%; display: flex; align-items: center">
+        <div class="container">
+            <h1 class="display-3" style="font-size: 3.5rem"><i class="fas fa-search fa-sm"></i> <b>Page not found</b>
+            </h1><br>
+            <h1 class="display-4" style="font-size: 2rem">Maybe the page you are looking for is <b>somewhere</b> out
+                there.</h1>
+            <h1 class="display-4" style="font-size: 2rem"><b>But it certainly is not here.</b></h1>
+        </div>
+    </div>
+
+</div>
+
+
+<?php
+/*
+ * Include the JavaScript libraries for Bootstrap.
+ */
+require_once $_SERVER["DOCUMENT_ROOT"] . "/resources/html/BootstrapBody.html";
+?>
+</body>
+</html>
