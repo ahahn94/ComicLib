b99fc0ac26d6c5d16c19dcb99a54d62e1f8aadec
André Hahn <ahahn94@outlook.com>
2024-04-26 18:06:30 +0200

    Refactoring of Connection.php

--- src/php_includes/Database/Management/Connection.php
+++ src/php_includes/Database/Management/Connection.php
@@ -7,6 +7,7 @@
 require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/Configuration/Configuration.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/Database/Management/Initialization.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/Logging/Logging.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/Exceptions/EmptyOrUnreadableConfigurationException.php";
 
 /**
  * Class Connection
@@ -15,7 +16,12 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/php_includes/Logging/Logging.php";
 class Connection
 {
     // Singleton connection object.
-    private static $instance = null;
+    private static ?PDO $instance = null;
+
+    private static array $pdoOptions;
+    private static string $dsn;
+    private static string $dbUsername;
+    private static string $dbPassword;
 
     /**
      * Get the PDO connection.
@@ -23,31 +29,70 @@ class Connection
      * Will initialize the database tables if not already initialized.
      * @return PDO connection.
      */
-    public static function getInstance()
+    public static function getInstance(): ?PDO
     {
         // Check if connection is not yet initialized.
         if (!isset(self::$instance)) {
-            $config = Configuration::getConfiguration(); // Get the database connection config.
-            if (!empty($config)) {
-                // If reading config was successful, continue.
-                $config = $config["Database"]; // Reduce config to database part.
-                $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION; // Make SQL-errors raise exceptions.
-                $pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND] = "Set Names utf8"; // Force utf8 charset.
-                try {
-                    self::$instance = new PDO("mysql:host=" . $config["MYSQL_HOST"] . ";dbname=" . $config["MYSQL_DATABASE"] .
-                        ";", $config["MYSQL_USER"], $config["MYSQL_PASSWORD"], $pdo_options);
-                    Initialization::check();
-                } catch (Exception $e) {
-                    // Error handling if error while connecting to database.
+            return self::initialize();
+        } else {
+            return self::$instance;
+        }
+    }
+
+    private static function initialize(): ?PDO
+    {
+        try {
+            self::configurePdoOptions();
+            self::$instance = new PDO(self::$dsn, self::$dbUsername, self::$dbPassword, self::$pdoOptions);
+            return self::$instance;
+        } catch (Exception $e) {
+            switch (get_class($e)) {
+                case EmptyOrUnreadableConfigurationException::class:
+                    Logging::logError("Connecting to the database failed! Couldn't read configuration file or it is empty.");
+                    return null;
+                default:
                     Logging::logError("Could not connect to database:");
                     Logging::logError($e->getMessage());
                     print ($e->getMessage());
-                    return self::$instance;
-                }
-            } else {
-                Logging::logError("Connecting to the database failed!");
+                    return null;
             }
         }
-        return self::$instance;
+    }
+
+    /**
+     * @throws EmptyOrUnreadableConfigurationException
+     */
+    private static function configurePdoOptions(): void
+    {
+        if (self::optionsAlreadyConfigured()) return;
+
+        $dbConfig = self::getDbConfig();
+
+        self::$pdoOptions = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,        // Make SQL-errors raise exceptions.
+            PDO::MYSQL_ATTR_INIT_COMMAND => "Set Names utf8"    // Force utf8 charset.
+        );
+
+        self::$dsn = "mysql:host={$dbConfig["MYSQL_HOST"]};dbname={$dbConfig["MYSQL_DATABASE"]};";
+        self::$dbUsername = $dbConfig["MYSQL_USER"];
+        self::$dbPassword = $dbConfig["MYSQL_PASSWORD"];
+
+    }
+
+    /**
+     * @throws EmptyOrUnreadableConfigurationException
+     */
+    private static function getDbConfig(): array
+    {
+        $config = Configuration::getConfiguration(); // Get the database connection config.
+        if (empty($config)) {
+            throw new EmptyOrUnreadableConfigurationException();
+        }
+        return $config["Database"];
+    }
+
+    private static function optionsAlreadyConfigured(): bool {
+        return !empty(self::$pdoOptions) && !empty(self::$dsn) && !empty(self::$dbUsername) && !empty(self::$dbPassword);
     }
 }
+
---
+++ src/php_includes/Exceptions/EmptyOrUnreadableConfigurationException.php
@@ -0,0 +1,5 @@
+<?php
+
+class EmptyOrUnreadableConfigurationException extends Exception
+{
+}
